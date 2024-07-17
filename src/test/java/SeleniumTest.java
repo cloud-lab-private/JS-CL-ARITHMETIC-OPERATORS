02287b0519cc091bb3ba174eb02aa1d654e7e0dc
Revathi-Kamineni <104614509+Revathi-Kamineni@users.noreply.github.com>
2024-07-17 14:29:02 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -5,14 +5,9 @@ import java.io.File;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.ie.InternetExplorerOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -109,42 +104,11 @@ public class SeleniumTest {
 
     @AfterEach
     public void tearDown() {
-        if (webDriver != null) {
+       
             webDriver.quit();
-        }
+        
     }
 }
 
 
-class BrowserUtils {
-    public static String getWebDriverName() {
-        String[] browsers = { "chrome", "firefox", "edge", "ie" };
 
-        for (String browser : browsers) {
-            try {
-                switch (browser) {
-                    case "chrome":
-                        WebDriverManager.chromedriver().setup();
-                        new ChromeDriver().quit();
-                        break;
-                    case "firefox":
-                        WebDriverManager.firefoxdriver().setup();
-                        new FirefoxDriver().quit();
-                        break;
-                    case "edge":
-                        WebDriverManager.edgedriver().setup();
-                        new EdgeDriver().quit();
-                        break;
-                    case "ie":
-                        WebDriverManager.iedriver().setup();
-                        new InternetExplorerDriver().quit();
-                        break;
-                }
-                return browser;
-            } catch (Exception e) {
-                continue;
-            }
-        }
-        return "Unsupported Browser";
-    }
-}
